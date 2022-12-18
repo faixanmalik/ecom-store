ee24f1cd59553fdd8b2abd810a502e0ea4a17984
faixan554 <faixan.malik554@gmail.com>
2022-12-18 18:18:48 +0500

    add database models

---
+++ models/User.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+    userId: String,
+    firstname:{type: String, required: true},
+    lastname:{type: String, required: true},
+    email: {type: Number, required: true},
+    password: {type: String, default: 'Pending', required: true},
+  },{timestamps:true});
+   
+export default mongoose.model("user", userSchema);
