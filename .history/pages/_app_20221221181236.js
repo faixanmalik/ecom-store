66be2631d7bbc1833c4331d45a02c97cd742a69b
faixan554 <faixan.malik554@gmail.com>
2022-12-21 18:40:41 +0500

    fixing some bugs in my store

---
+++ .history/pages/_app_20221221181236.js
@@ -0,0 +1,107 @@
+import { useState , useEffect } from 'react'
+import Footer from '../components/Footer'
+import Navbar from '../components/Navbar'
+import '../styles/globals.css'
+
+
+// React Toastify
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+function MyApp({ Component, pageProps }) {
+
+  let [cart, setCart] = useState({})
+  let [subTotal, setSubTotal] = useState(0)
+
+
+
+  //  Use Effect for retain same items in shopping Cart
+  useEffect(() => {
+    try {
+      if (localStorage.getItem("cart")) {
+        setCart(JSON.parse(localStorage.getItem("cart")));
+        saveCart(JSON.parse(localStorage.getItem("cart")));
+      }
+    } catch (error) {
+      console.error(error);
+      localStorage.clear();
+    }
+  }, [])
+  
+
+
+
+  //  saveCart is used to store cart items in local storage of user
+  const saveCart = (myCart) => {
+    localStorage.setItem("cart", JSON.stringify(myCart))
+
+  //   // function uses to subtotal
+    let subt = 0;
+    let keys = Object.keys(myCart);
+    for (let i = 0; i < keys.length; i++) {
+      subt +=  myCart[keys[i]].price * myCart[keys[i]].qty;
+    }
+    setSubTotal(subt)
+  }
+
+
+  // add to Cart function like increase quantity of items in cart
+  const addcart = () => toast("Item is added in your Cart!");
+  const removecart = () => toast("Item is added in your Cart!");
+
+
+
+  const addToCart = (itemCode, name, qty, price, size, variant) =>{
+    addcart();
+    let newCart = cart;
+    if(itemCode in cart){
+      newCart[itemCode].qty= cart[itemCode].qty + qty;
+    }
+    else{
+      newCart[itemCode]= {qty:1, name, price, size, variant}   
+    }
+    setCart(newCart);
+    saveCart(newCart);
+  }
+
+
+
+
+  // Remove From Cart function like decrease quantity of items in cart
+  const removeFromCart = (itemCode, name, qty, price, size, variant) =>{
+    let newCart = cart;
+    if(itemCode in cart){
+      newCart[itemCode].qty= cart[itemCode].qty - qty;
+    }
+     if (newCart[itemCode].qty <=0){
+      delete newCart[itemCode];
+     }
+    setCart(newCart);
+    saveCart(newCart);
+  } 
+
+
+
+  
+  // clear cart is used to clear all items in cart
+  const clearCart = () => {
+    setCart({})
+    saveCart({})
+  }
+
+
+
+
+
+
+
+
+  return <>
+  <Navbar cart={cart} addToCart={addToCart} removeFromCart={removeFromCart} clearCart={clearCart} subTotal={subTotal} />
+  <Component cart={cart} addToCart={addToCart} removeFromCart={removeFromCart} clearCart={clearCart} subTotal={subTotal}  {...pageProps} />
+  <Footer/>
+  </> 
+
+}
+
+export default MyApp
---
+++ .history/pages/hoodies_20221221182949.js
@@ -0,0 +1,50 @@
+import React from 'react'
+import Link from 'next/link'
+import Product from '../models/Product';
+import mongoose from "mongoose";
+
+function Hoodies({ product }) {
+  return (
+  <section className="text-gray-600 body-font">
+    <div className="container px-5 pt-5 pb-20 mx-auto">
+      <div className="flex flex-wrap px-10 lg:px-20 space-y-20">
+        <div></div>
+
+        {product.length === 0 && <div className="">Sorry! Currently Stock Unavailble right now. Please</div>
+        {product.map((item)=>{
+        return <div key={item._id} className="w-64 mx-auto">
+          <div className="block h-72 rounded overflow-hidden">
+            <Link href={`/product/${item.slug}`}><img alt="ecommerce" className="w-full h-full block" src={item.img}/></Link>
+          </div>
+          <div className="mt-4 text-center sm:text-left">
+            <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">{item.title}</h3>
+            <h2 className="text-gray-900 title-font text-lg font-medium">Lets enjoy!</h2>
+            <p className="mt-1">${item.price}</p>
+          </div>
+        </div>})}
+        
+
+      </div>
+    </div>
+  </section>
+  )
+}
+
+
+
+export async function getServerSideProps() {
+  if (!mongoose.connections[0].readyState){
+    await mongoose.connect(process.env.MONGO_URI)
+  }
+  let product = await Product.find({category: 'hoodies'})
+
+ 
+  // Pass data to the page via props
+  return {
+     props: { product: JSON.parse(JSON.stringify(product)) } 
+    }
+}
+
+
+
+export default Hoodies
