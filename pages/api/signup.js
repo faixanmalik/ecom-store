fd0fd8396231a3798818c06907c7d9c1d2aa4c6d
faixan554 <faixan.malik554@gmail.com>
2022-12-22 16:42:05 +0500

    preparing for signup apis

---
+++ pages/api/signup.js
@@ -0,0 +1,14 @@
+import User from '../../models/User'
+import connectDb from '../../middleware/mongoose'
+
+const handler = async (req,res)=>{
+    if (req.method == 'POST'){
+
+        res.status(200).json({ success: "Item added Succesfully!" })
+    }
+    else{
+        res.status(400).json({ error: "This method is not allowed!" })
+    }
+
+}
+export default connectDb(handler);
